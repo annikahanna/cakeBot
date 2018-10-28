7fbe24a14a79eec51025d9ef4d7cecfb880f8252
Annika <annika.schatz21@web.de>
2018-10-28 16:06:02 +0100

    added data layer. Loging of data is now available

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ const {
     Extra,
     Markup
 } = require('telegraf');
-
+const dataService = require('./dataService');
 const config = require('./config');
 
 const bot = new Telegraf(config.botToken);
@@ -14,10 +14,32 @@ bot.telegram.getMe().then((botInfo) => {
     console.log("Initialized", botInfo.username);
 });
 
+dataService.loadUsers();
+
+
 bot.command('start', ctx => {
+    dataService.registerUser(ctx);
     ctx.replyWithHTML(
         "Hallo, ich bin der Bot zur durchgehenden Kuchenversorgung.");
 });
+
+bot.command('setsize', ctx => {
+    const message = ctx.message.text.split(" ");
+    if (message.length > 1) {
+        var groupSize = "";
+        for (let i = 1; i < message.length; i++) {
+            groupSize = groupSize + " " + message[i];
+        }
+        groupSize = groupSize.trim();
+
+        ctx.replyWithHTML(
+            "Die Gruppengröße wurde auf " + groupSize + " gesetzt");
+        dataService.setGroupSize(ctx.chat.id, groupSize);
+    }
+    else {
+        "Du musst eine Zahl mitangeben um die Gruppengröße zu setzen"
+    }
+})
 
 //Das Prinzip ist relativ simpel...es soll ein faires Kuchenbacksystem entstehen. Jeder hat die Möglichkeit Kuchen mitzubringen und bekommt dafür Kuchen gutgeschrieben.
 // Dafür muss zum Anfang die Größe des Kuchenzirkels festgelegt werden. Wie oft ich "kostenlos" Kuchen essen darf hängt von dieser Größe ab. Zirkelgröße = g; kostenloser Kuchen = k; n = wie oft Kuchen mitgebracht
@@ -25,30 +47,33 @@ bot.command('start', ctx => {
 //Am Anafang bekommt jeder ein default Kontigent von g
 // Es soll eine Kuchenwunschliste geben, die abgearbeitet werden kann (vielleicht mit API Anbindung)
 
-bot.command('takePiece', ctx => {
-    var freeCake = 8;
-    if (freeCake >0){
-        freeCake--;
+bot.command('takepiece', ctx => {
+    var freeCake = dataService.getFreeCake(ctx.chat.id, ctx.from.id);
+    if (parseInt(freeCake) > 0) {
+        freeCake = parseInt(freeCake) - 1;
         ctx.replyWithHTML(
-            ctx.from.first_name + " (@" + ctx.from.username + ") möchte gerne ein Stück Kuchen haben. Du hast noch "+ freeCake+ " freie Stücke zur Verfügung");
+            ctx.from.first_name + " (@" + ctx.from.username + ") möchte gerne ein Stück Kuchen haben. Du hast noch " + freeCake + " freie Stücke zur Verfügung");
+        dataService.setFreeCake(ctx.chat.id, ctx.from.id, freeCake);
     }
-    else{
+    else {
         ctx.replyWithHTML(
             "Du hast leider dein Kontigent aufgebraucht um Kuchen zu konsumieren. Um es aufzufüllen musst du entweder eine Zutat zum Backen sponsoren oder Kuchen mitbringen");
     }
 
 });
 
-bot.command('shareCake', ctx => {
-    var freeCake = 8;
-    freeCake = freeCake+8;
-        ctx.replyWithHTML(
-            ctx.from.first_name + " (@" + ctx.from.username + ") stellt Kuchen zur Verfügung. Wenn du ein Stück haben möchtest gib bitte /takePiece ein. "+ ctx.from.first_name +
-            " dir stehen "+freeCake+" freie Stücke zur Verfügung");
+bot.command('sharecake', ctx => {
+    var freeCake = dataService.getFreeCake(ctx.chat.id, ctx.from.id);
+    var groupSize = dataService.getGroupSize(ctx.chat.id);
+    freeCake = parseInt(freeCake) + parseInt(groupSize);
+    dataService.setFreeCake(ctx.chat.id, ctx.from.id, freeCake);
+    ctx.replyWithHTML(
+        ctx.from.first_name + " (@" + ctx.from.username + ") stellt Kuchen zur Verfügung. Wenn du ein Stück haben möchtest gib bitte /takepiece ein. " + ctx.from.first_name +
+        ", dir stehen " + freeCake + " freie Stücke zur Verfügung");
 });
 
-bot.command('sponsor', ctx => {
 
+bot.command('sponsor', ctx => {
     const message = ctx.message.text.split(" ");
     if (message.length > 1) {
         ingredient = "";
@@ -58,16 +83,16 @@ bot.command('sponsor', ctx => {
         ingredient = ingredient.trim();
     }
 
-    var freeCake = 8;
-    freeCake = freeCake+4;
+    var freeCake = dataService.getFreeCake(ctx.chat.id, ctx.from.id);
+    var groupSize = dataService.getGroupSize(ctx.chat.id);
+    freeCake = parseInt(freeCake) + 0.5 * parseInt(groupSize);
+    dataService.setFreeCake(ctx.chat.id, ctx.from.id, freeCake);
     ctx.replyWithHTML(
-        ctx.from.first_name + " (@" + ctx.from.username + ") stellt die Zutat "+ingredient+ " zur Verfügung. Wenn du die Zutat benötigst gib bitte /take ein."+ ctx.from.first_name +
-        " dir stehen nun "+freeCake+" freie Stücke zur Verfügung");
+        ctx.from.first_name + " (@" + ctx.from.username + ") stellt die Zutat " + ingredient + " zur Verfügung. Wenn du die Zutat benötigst gib bitte /take ein. " + ctx.from.first_name +
+        ", dir stehen nun " + freeCake + " freie Stücke zur Verfügung");
 });
 
 bot.command('take', ctx => {
-    var freeCake = 8;
-    freeCake = freeCake+8;
     ctx.replyWithHTML(
         ctx.from.first_name + " (@" + ctx.from.username + ") nimmt die Zutat");
 });
